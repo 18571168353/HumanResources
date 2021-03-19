691f14fcd10c6ef4956888fa9d703535a0cca165
2730747608@qq.com <2730747608@qq.com>
2021-03-19 19:43:47 +0800

    主页左侧导航样式完成

---
+++ src/permission.js
@@ -0,0 +1,35 @@
+// 权限拦截 导航守卫 路由守卫
+import router from '@/router' // 引入路由实例
+import store from '@/store' // 引入vuex store实例
+const whiteList = ['/login', '/404'] // 定义一个白名单
+import NProgress from 'nprogress' // 引入一份进度条插件
+import 'nprogress/nprogress.css' // 引入进度条样式
+// 前置路由
+router.beforeEach((to, from, next) => {
+  NProgress.start() // 开启进度条
+  // 如果有token
+  if (store.getters.token) {
+    // 如果访问的是登录页
+    if (to.path === '/login') {
+      // 直接跳到主页
+      next('/')
+    } else {
+      // 否则直接放行
+      next()
+    }
+  } else {
+    // 没有token
+    if (whiteList.indexOf(to.path) > -1) {
+      // 如果访问的是白名单里面的内容 直接放行
+      next()
+    } else {
+      // 如果不是,跳到登录页
+      next('/login')
+    }
+  }
+  NProgress.done() // 手动强制关闭一次  为了解决 手动切换地址时  进度条的不关闭的问题
+})
+// 后置路由
+router.afterEach(() => {
+  NProgress.done()
+})
